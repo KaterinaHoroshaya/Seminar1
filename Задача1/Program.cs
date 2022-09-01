1094cc982df184d6bfeac5d6d1ba1dc65211f658
Katerina <m-a-c-u-k-a@mail.ru>
2022-09-01 20:13:45 +0700

    Задача 1

---
+++ Задача1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("input number A: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("input number B: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if(Math.Pow(numberA,2)==numberB)
+{
+    Console.WriteLine("A является квадратом B");
+}
+else
+{
+    Console.WriteLine("A не является квадратом B");
+}
