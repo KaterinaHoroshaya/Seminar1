16a1305490d4789d2ce6c2a67e6b214adb9267d7
Katerina <m-a-c-u-k-a@mail.ru>
2022-09-01 20:33:34 +0700

    Задача 2

---
+++ Задача2/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.Write("Введите номер дня недели: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+if (A==1)
+{
+    Console.WriteLine("Monday");
+}
+else if (A==2)
+{
+    Console.WriteLine("Tuesday");
+}
+else if (A==3)
+{
+    Console.WriteLine("Wednesday");
+}
+else if (A==4)
+{
+    Console.WriteLine("Thursday");
+}
+else if (A==5)
+{
+    Console.WriteLine("Friday");
+}
+else if (A==6)
+{
+    Console.WriteLine("Saturday");
+}
+else if (A==7)
+{
+    Console.WriteLine("Sunday");
+}
+else 
+{
+    Console.WriteLine("Неверное число");
+}
